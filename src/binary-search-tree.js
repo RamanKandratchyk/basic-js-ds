b768d1a7bb2274994a232ed6fb7dc2b1c194de55
Raman Kandratchyk <kondrator@gmail.com>
2022-09-02 17:10:11 +0300

    init: remove method in binary-search-tree task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -111,10 +111,73 @@ class BinarySearchTree {
   }
 
   remove(data) {
-    this.current = this.find(data);
-    if (this.current === null) return false;
+    let currentLeft = false, currentRight = false;
+    if (this.list === null) return false;
 
-    // implement 3 cases here
+    if (this.list.data === data) {
+      if (this.list.left === null && this.list.right === null) this.list = null;
+      if (this.list.left === null && this.list.right !== null) this.list = this.list.right;
+      if (this.list.left !== null && this.list.right === null) this.list = this.list.left;
+    };
+
+    this.current = this.list;
+
+    do {
+      if (data < this.current.data) {
+        if (this.current.left && this.current.left.data === data) {
+          this.delNode = this.current.left;
+          currentLeft = true;
+          break;
+        };
+
+        if (this.current.left === null) {
+          return false;
+        } else {
+          this.current = this.current.left;
+        };
+      };
+
+      if (this.current.data < data) {
+        if (this.current.right && this.current.right.data === data) {
+          this.delNode = this.current.right;
+          currentRight = true;
+          break;
+        };
+
+        if (this.current.right === null) {
+          return false;
+        } else {
+          this.current = this.current.right;
+        };
+      };
+    } while (true);
+
+    // if (currentLeft && this.current.left.left === null && this.current.left.right === null) this.current.left = null;
+    // if (currentRight && this.current.right.left === null && this.current.right.right === null) this.current.right = null;
+
+    // if (currentLeft && this.current.left.left && this.current.left.right === null) this.current.left = this.current.left.left;
+    // if (currentLeft && this.current.left.left === null && this.current.left.right) this.current.left = this.current.left.right;
+
+    // if (currentRight && this.current.right.left && this.current.right.right === null) this.current.right = this.current.right.left;
+    // if (currentRight && this.current.right.left === null && this.current.right.right) this.current.right = this.current.right.right;
+
+    if (currentLeft && this.delNode.left === null && this.delNode.right === null) this.current.left = null;
+    if (currentRight && this.delNode.left === null && this.delNode.right === null) this.current.right = null;
+
+    if (currentLeft && this.delNode.left && this.delNode.right === null) this.current.left = this.delNode.left;
+    if (currentLeft && this.delNode.left === null && this.delNode.right) this.current.left = this.delNode.right;
+
+    if (currentRight && this.delNode.left && this.delNode.right === null) this.current.right = this.delNode.left;
+    if (currentRight && this.delNode.left === null && this.delNode.right) this.current.right = this.delNode.right;
+
+
+    if (this.delNode.right.left === null) {
+      if (currentLeft) this.current.left = this.delNode.right;
+      if (currentRight) this.current.right = this.delNode.right;
+    };
+
+
+    this.curNode = this.delNode;
 
   }
 
